4743f82e0d709c9b955d37841c5f9e8413a7df0d
Ankit <ankit628792@gmail.com>
2020-11-03 20:43:02 +0530

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,6 +12,7 @@ function Sidebar() {
 
     return (
         <div className="sidebar">
+         <center><p>Designed &amp; Developed by AK</p></center> 
             <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
             <SidebarOption Icon={HomeIcon} title="Home" />
             <SidebarOption Icon={SearchIcon} title="Search" />
